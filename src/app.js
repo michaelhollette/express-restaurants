69a428b4f679f08b7821ef20cf9a3241d1675bbd
mh2011-maisie <mh2011.music@gmail.com>
2024-10-14 16:42:57 +0100

    Finished

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
+app.get("/restaurants", async (request, response) =>{
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants)
+})
 
 
 
