58dd5bb227c0dd64a44ad7754771031801917ec2
mh2011-maisie <mh2011.music@gmail.com>
2024-10-15 13:35:38 +0100

    Part 2 and Part 3

--- src/app.js
+++ src/app.js
@@ -8,8 +8,32 @@ const db = require("../db/connection");
 app.get("/restaurants", async (request, response) =>{
     const restaurants = await Restaurant.findAll();
     response.json(restaurants)
+});
+app.get("/restaurants/:id", async (request, response) =>{
+    let id = request.params.id;
+    let restaurant = await Restaurant.findByPk(id);
+    response.json(restaurant);
+    
 })
 
+app.use(express.json())
+app.use(express.urlencoded());
 
+app.post("/restaurants", async (req,res) =>{
+    await Restaurant.create(req.body)
+    restaurants = await Restaurant.findAll();
+    res.json(restaurants)
+});
 
+app.put("/restaurants/:id", async (req,res) => {
+    await Restaurant.update(req.body, {where: {id: req.params.id}});
+    let restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+})
+
+app.delete("/restaurants/:id", async (req,res) =>{
+    await Restaurant.destroy({where: {id: req.params.id}});
+    let restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+})
 module.exports = app;
